b59f372e020c7b1a137f2b800764d0d4764c41e1
Татьяна <veselovskayt@mail.ru>
2022-01-03 16:12:57 +0700

    Добавьте файлы проекта.

---
+++ Task_22/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Введите размер массива: ");
+            n = Int32.Parse(Console.ReadLine());
+            int[] mass = new int[n];
+
+
+            Task task1 = new Task(() => insert_mass());
+            Task task2 = task1.ContinueWith(insert_mas => Max());
+            Task task3 = task1.ContinueWith(insert_mas => Summ());
+
+            task1.Start();
+            task2.Wait();
+            task3.Wait();
+            Console.ReadKey();
+
+            void Summ()
+            {
+                int temp = 0;
+                for (int j = 0; j < n; j++) temp = temp + mass[j];
+                Console.WriteLine("Сумма массива = " + temp);
+            }
+            void Max()
+            {
+                int temp;
+                temp = mass.Max();
+                Console.WriteLine("Максимальное значение массива = " + temp);
+            }
+            void insert_mass()
+            {
+                Random r = new Random();
+                for (int i = 0; i < n; i++) mass[i] = r.Next(0, 500);
+                Console.WriteLine("Массив заполнен");
+                foreach (int k in mass) Console.Write(" " + k);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Task_22A/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_22A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размер массива: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Func<object, int[]> func1 = new Func<object, int[]>(GetArray);
+            Task<int[]> task1 = new Task<int[]>(func1, n);
+
+            Action<Task<int[]>> action = new Action<Task<int[]>>(PrintArray);
+            Task task2 = task1.ContinueWith(action);
+
+            Action<Task<int[]>> func2 = new Action<Task<int[]>>(Summ);
+            Task task3 = task1.ContinueWith(action);
+
+            Action<Task<int[]>> func3 = new Action<Task<int[]>>(Max);
+            Task task4 = task1.ContinueWith(action);
+
+            task1.Start();
+            task2.Wait();
+            task3.Wait();
+            task4.Wait();
+
+            Console.ReadKey();
+        }
+        static int[] GetArray(object a)
+        {
+            int n = (int)a;
+            int[] array = new int[n];
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = random.Next(0, 200);
+            }
+            return array;
+        }
+        static void PrintArray(Task<int[]> task)
+        {
+            int[] array = task.Result;
+            for (int i = 0; i < array.Count(); i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+        }
+        static void Summ(Task<int[]> task)
+        {
+            int t = 0;
+            int[] array = task.Result;
+            for (int j = 0; j < array.Count(); j++) t = t+ array[j];
+            Console.WriteLine("Сумма массива = " + t);
+        }
+        static void Max(Task<int[]> task)
+        {
+            int t;
+            int[] array = task.Result;
+            t = array.Max();
+            Console.WriteLine("Максимальное значение массива = " + t);
+        }
+    }
+}
